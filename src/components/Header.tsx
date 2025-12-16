703851d78ca00db51d817157eb4a1e7097c7f32f
Mayank Banoula <101325491+banoulamayank@users.noreply.github.com>
2025-12-16 15:52:22 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,7 +18,7 @@ const Header = () => {
         <div className="flex items-center justify-between">
           <div className="flex items-center space-x-2">
             <img
-              src="logo.png"
+              src="src/components/logo.png"
               alt="AI"
               className="h-12 w-auto"
             />
