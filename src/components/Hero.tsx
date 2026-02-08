7c5b7c96529d95539632f79c88365bcc80c61f1e
Mayank Banoula <101325491+banoulamayank@users.noreply.github.com>
2026-02-08 23:55:44 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -168,6 +168,24 @@ const Hero = () => {
       duration: '14:50',
       publishedAt: '2026-01-24T00:00:00Z',
     },
+     {
+      id: '20',
+      title: 'Artificial Intelligence Course | AI Tools Course',
+      description: 'Learn AI In 2 Hours',
+      youtubeId: 'EtDbtfahmvM',
+      category: 'Productivity Tutorials',
+      duration: '01:33:18',
+      publishedAt: '2026-02-07T00:00:00Z',
+    },
+    {
+      id: '21',
+      title: 'AI Tool Made my Tutorial Video In Just 10 Mins',
+      description: 'Create tutorials without Voiceover',
+      youtubeId: 'Zxs9hOzusrU',
+      category: 'Productivity Tutorials',
+      duration: '09:42',
+      publishedAt: '2026-02-08T00:00:00Z',
+    },
   ];
 
   // Get top 5 videos from ALL section (same logic as VideoTutorials ALL category)
